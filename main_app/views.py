b6ec46ae8a11d621e661b076a04766443f47ea47
Sunjang1009 <sunjang1009@gmail.com>
2023-06-21 22:08:30 -0500

    basic setup connected with static

--- main_app/views.py
+++ main_app/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
+from django.views.generic.base import TemplateView
+
 
 # Create your views here.
 
-class Home(View):
-    def get(self, request):
-        return HttpResponse("Finch_Collector Home")
+class Home(TemplateView):
+    template_name = 'home.html'
 
-class About(View):
-    def get(self, request):
-        return HttpResponse("Finch_Collector About")
+class About(TemplateView):
+    template_name = 'about.html'
+    
+    
 
