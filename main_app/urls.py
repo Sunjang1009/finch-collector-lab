b7af31511c23908cd3ef37fee86409a408c7958d
Sunjang1009 <sunjang1009@gmail.com>
2023-06-23 21:03:42 -0500

    set up mocked data in views.py file

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.Home.as_view(), name="home"),
     path('about/', views.About.as_view(), name="about"),
+    path('corgis/', views.CorgisList.as_view(), name="corgis_list"),
 ]
+
+
 
 
--- main_app/views.py
+++ main_app/views.py
@@ -11,6 +11,30 @@ class Home(TemplateView):
 
 class About(TemplateView):
     template_name = 'about.html'
-    
-    
+
+class Corgis:
+    def __init__(self, name, mixes, image, bio):
+        self.name = name
+        self.mixes = mixes
+        self.image = image
+        self.bio = bio
+
+corgis = [
+    Corgis("Corgsky", "Husky X Corgi",
+    "https://www.101dogbreeds.com/wp-content/uploads/2018/07/Corgsky.jpg",
+    "It is friendly, loyal, and gentle, inheriting the fun-loving nature of both its parents, which makes it a great family pet well-suited for all ages." ),
+    Corgis("Corman Shepherd", "German Shepherd X Corgi", 
+    "https://www.101dogbreeds.com/wp-content/uploads/2016/11/Corman-Shepherd.jpg",
+    "Intelligent, smart, and easy to please, they replicate their German Shepherd parent when it comes to being loyal, protective, and dedicated."),
+    Corgis("Beagi", "Beagle X Corgi", 
+    "https://www.101dogbreeds.com/wp-content/uploads/2015/09/Beagi.jpg",
+    "The drooping ears of the Beagle and the striped appearance of the Corgi (on the eyes and nose) make these amicable, intelligent, and loyal dogs immensely adorable. Most of them even display the strong smelling and hunting instincts of both their parents.")
+]
+
+class CorgisList(TemplateView):
+    template_name = "corgis_list.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["corgis"] = corgis
+        return context
 
