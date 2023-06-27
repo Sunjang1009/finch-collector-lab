b29da99d51769bcba9ceb3b45a10de509bf0d0e3
Sunjang1009 <sunjang1009@gmail.com>
2023-06-26 21:27:44 -0500

    updated Dog gender in model file

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Corgi
+from .models import Corgi, Dog
 
 # Register your models here.
 admin.site.register(Corgi)
+admin.site.register(Dog)
+
 
--- main_app/models.py
+++ main_app/models.py
@@ -16,3 +16,21 @@ class Corgi(models.Model):
         return self.name
 
 
+class Dog(models.Model):
+    name = models.CharField(max_length=100)
+    weight = models.IntegerField(default=10)
+    color = models.CharField(max_length=100)
+    Female = "Female"
+    Male = "Male"
+    DOG_GENDER_CHOICES = [
+        (Female,"Female"),
+        (Male, "Male"),
+    ]
+    gender = models.CharField(max_length=10,
+    choices=DOG_GENDER_CHOICES,
+    default="female",
+    )
+    corgi = models.ForeignKey(Corgi, on_delete=models.CASCADE, related_name="dogs")
+    def __str__(self):
+        return self.name
+
